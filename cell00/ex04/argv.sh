842ad1924146022e7d21e5da7e35776152d65d93
Thun_Porawat <96039261+xXBiosoftXx@users.noreply.github.com>
2024-09-09 22:38:51 +0700

    Create argv.sh

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" - eq 0 ]; then
+  echo "No arguments supplied"
+  exit 1
+fi
+
+if [ "$#" -lt 1 ]; then
+  exit 1
+fi
+
+count=0
+for arg in "$@"; do
+  echo "$arg"
+  count=$((count + 1))
+  if [ "$count" -eq 3 ]; then
+    break
+  fi
+done
